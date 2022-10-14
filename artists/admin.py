a206cdccc5f6d56d9fcbb67130539a3d8d8709e9
3RB16 <muhammad3rbi@gmail.com>
2022-10-14 11:32:24 +0200

    create django admin

--- artists/admin.py
+++ artists/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Artist
 # Register your models here.
+
+class ArtistDataAdmin(admin.ModelAdmin):
+    list_display = ('stage_name' , 'social_link' , 'count_albums')
+
+admin.site.register(Artist, ArtistDataAdmin)
--- artists/models.py
+++ artists/models.py
@@ -4,8 +4,14 @@ from django.db import models
 class Artist(models.Model):
     stage_name = models.CharField(max_length = 100, unique = True, blank = False, null = False)
     social_link = models.URLField(max_length = 200, blank = True, null = False)
+    
+    @property
+    def count_albums(self):
+        return self.album_set.filter(admin_approved = True).count()
+
     class Meta:
+        db_table = 'artists'
         ordering = ['stage_name']
-        
+    
     def __str__(self):
         return self.stage_name
