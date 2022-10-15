3e034ed2b0c0f73d11ff7c5797db581982769cc8
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2022-10-15 19:54:10 +0200

    create albums from artist admin
    Allow the admin to create albums for the artist from the artist's editing form

--- artists/admin.py
+++ artists/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models import Artist
+from albums.models import Album
+
 # Register your models here.
+class AlbumDataAdmin(admin.StackedInline):
+    model = Album
+    extra = 0
 
 class ArtistDataAdmin(admin.ModelAdmin):
-    list_display = ('stage_name' , 'social_link' , 'count_albums')
+    inlines = [AlbumDataAdmin]
+    list_display = ['stage_name' , 'social_link' , 'count_albums']
 
 admin.site.register(Artist, ArtistDataAdmin)
